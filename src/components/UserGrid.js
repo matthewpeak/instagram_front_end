6e972ecb31f9ef98d126536120313eceb02c5415
matthewpeak <matthewpeak@gmail.com>
2021-07-01 16:05:15 -0400

    basic grid logic

--- src/components/UserGrid.js
+++ src/components/UserGrid.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import '../Grid.css'
 const UserGrid =(props)=>{
 
     return(
-        <div>
-            Hello {props.user}
+        <div className='grid-container'>
+            <div className='picture-container'>A</div>
+            <div className='picture-container'>A</div>
+            <div className='picture-container'>A</div>
+            <div className='picture-container'>A</div>
+            <div className='picture-container'>A</div>
+            <div className='picture-container'>A</div>
+            <div className='picture-container'>A</div>
+            <div className='picture-container'>A</div>
+            <div className='picture-container'>A</div>
         </div>
     )
 }
