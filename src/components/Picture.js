5223126558718710f0914a0883679c2226adefa5
matthewpeak <matthewpeak@gmail.com>
2021-07-06 12:39:09 -0400

    picture css

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,10 +1,11 @@
 import React from 'react' 
+import '../Grid.css'
 
 const Picture = (props)=>{
   
     return(
     <div className='image-container'>
-     <img src={props.src}/>
+     <img className='image' src={props.src}/>
      <span>{props.caption}</span>
      <span> 'heart'</span>
      <span>{props.likecount}</span>
