910d7e70a58dc2e6525bbfd9fec267913f3265c8
matthewpeak <matthewpeak@gmail.com>
2021-07-07 09:52:34 -0400

    login menu added

---
+++ src/components/LogIn.js
@@ -0,0 +1,45 @@
+import {useState} from 'react'
+
+const Login =()=>{
+
+    const [values,setValues] =  useState({
+        username:'',
+        password:''
+    });
+
+    const handleChange =(e)=>{
+        const {name,value} = e.target
+
+        setValues({
+            ...values,
+            [name]:value
+        });
+    };
+
+    return(
+        <div className='form-content'>
+            <form className='form'>
+                <div className='form-input'>
+                    <label htmlFor='username' className='form-label'>
+                        Username
+                    </label>
+                    <input className='form-input' type='text' name='username' id='username' placeholder='enter your username' value={values.username} onChange={handleChange}>
+                    </input>
+                </div>
+                <div className='form-input'>
+              <label htmlFor='password' className='form-label'>
+                password
+              </label>
+                <input className='form-input' type='password' name='password' id='password' placeholder='enter your passwrod' value={values.password} onChange={handleChange} >
+                </input>
+             </div>
+             <button className='form-btn' type='submit'> 
+               Login
+             </button>
+            </form>
+        </div>
+    );
+
+}
+
+export default Login
