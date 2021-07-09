3537881015ae3ebfc909208089c72facbdc3b887
matthewpeak <matthewpeak@gmail.com>
2021-07-09 15:03:46 -0400

    svgs added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import SignUpForm from './components/SignUpForm'
 import UserInterface from './components/UserInterface';
 import UploadImage from './components/UploadImage'
+import NavBar from './components/NavBar';
+import { Fragment } from 'react';
 function App() {
   return (
+  <Fragment>
+   <NavBar/>
    <SignUpForm/>
-  //<UserInterface/>
-  // <UploadImage></UploadImage>
+  {/* //<UserInterface/>
+  // <UploadImage></UploadImage> */}
+  </Fragment>
   );
 }
 
