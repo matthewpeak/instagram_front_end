57291e6048f2c574328a8402373197b4555e6e2d
matthewpeak <matthewpeak@gmail.com>
2021-07-15 14:24:41 -0400

    comment display

--- src/components/CommentsDisplay.js
+++ src/components/CommentsDisplay.js
@@ -1,12 +1,14 @@
 import React,{useState,useMemo} from 'react'
-
-
+import Comment from './Comment'
 
 const CommentsDisplay=(props)=>{
+    
+    let comments = comments.sort((a,b)=>{return b.likes-a.likes})
+    comments = comments.map(comment=><Comment user_name={comment.user_name} likes={comments.like} liked={comment.liked} comment={comment.comment}/>)
 
     return(
-        <div>
-            
+        <div className='comments-display'>
+            {comments}
         </div>
     )
 
--- src/components/Picture.js
+++ src/components/Picture.js
@@ -4,8 +4,12 @@ import '../Grid.css'
 const Picture = (props)=>{
     
     const [inHover, setHover] = useState(false);
+    const [CommentsDisplay, setCommentDisplay] = useState(false)
+    
+    const handleCommentDisplay =()=>{
+        CommentsDisplay===false ? setCommentDisplay(true):setCommentDisplay(false);
+    }
 
-   
     return(
     <div className='image-container' onMouseEnter={() => setHover(true)}
     onMouseLeave={() => setHover(false)}>
@@ -21,9 +25,11 @@ const Picture = (props)=>{
              <span>{props.likecount}</span>
          </div>
          <div className='comments-info'>
+             <span onClick={handleCommentDisplay}>
              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
              <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd" />
              </svg>
+             </span>
              <span>{props.commentCount}</span>
          </div>
      </div>
